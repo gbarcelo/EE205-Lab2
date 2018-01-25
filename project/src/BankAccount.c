a2577b6518f2db445a699099e792338902324ca3
gbarcelo <gbarcelo@users.noreply.github.com>
2018-01-24 22:53:06 -1000

    original files from laulima

---
+++ project/src/BankAccount.c
@@ -0,0 +1,20 @@
+#include "BankAccount.h"
+
+struct BankAccount {
+    double balance;
+    double last_withdrawl;
+    double last_deposit;
+};
+
+/*
+ * Implement the definitions for the
+ * functions involving BankAccount from BankAccount.h here,
+ *
+ * Make sure to produce comments that look like this!
+ * (Try to copy the formatting.)
+ *
+ * For BankAccount_construct() and BankAccount_destroy(),
+ * if you do not remember malloc() and free(), review them
+ * by creating a toy program.
+ */
+
