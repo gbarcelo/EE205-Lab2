fb75a96bbea66f199e550ce66a54803fe3ab8688
gbarcelo <gbarcelo@users.noreply.github.com>
2018-01-24 23:25:34 -1000

    previous files were from lab1; fixed initial laulima files

---
+++ project/src/Person.hpp
@@ -0,0 +1,21 @@
+#ifndef PERSON_HPP
+#define PERSON_HPP
+
+#include "Point2D.hpp"
+
+// std::string is a class from the C++ standard
+// library that can be default-constructed
+// and constructed from a C-style string (string
+// literal like "Hello world")
+#include <string>
+
+// Make sure to use both Point2D and std::string
+// as member variables in the class below.
+class Person { 
+private:
+
+public:
+
+};
+
+#endif // PERSON_HPP
