0c5957a7f3efd4dc0ae32f94f2c13157fdf04257
chuckwolstein <wolstein@hawaii.edu>
2018-01-30 17:53:32 -1000

    chuck message

--- project/src/chuck.c
+++ project/src/chuck.c
@@ -1,3 +1,12 @@
-#include<stdio.h>
+#include<iostream>
+#include "Point2D.hpp"
+#include "Person.hpp"
+
+using namespace std;
+
+int millis = 3;
 int main()
-{}
+{
+cout << "My computer is slow" << millis <<endl;
+
+}
