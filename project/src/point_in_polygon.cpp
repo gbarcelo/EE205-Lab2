259ab11bca92618dfb2e700f7821e60986ef992d
Christine Ramos <christineramos12@gmail.com>
2018-02-01 10:19:18 -1000

    task3 test 1

--- project/src/point_in_polygon.cpp
+++ project/src/point_in_polygon.cpp
@@ -2,7 +2,7 @@
 #include "point_in_polygon.hpp"
 
 // Here is the original C version from Stack Overflow.
-// 
+//
 // Adapt it to use Person objects and a Person array
 // instead of just pure x/y coordinates.
 //
@@ -13,20 +13,35 @@
 //
 // You just need to have it use our C++ object type now!
 //
-int pnpoly(
-        int nvert,
-        float *vertx,
-        float *verty,
-        float testx,
-        float testy) {
+
+/*
+vertx, verty = loc
+testx, testy = pos
+nvert = nloc ????
+
+for (i = 0, j = nloc-1; i < nloc; j = i++){
+        if (((loc->y[i] > pos->y) !=
+                  (loc->y[j] > pos->y)) && (pos->x <
+                      (loc->x[j] - loc->x[i]) *
+                      (pos->y - loc->y[i]) /
+                      (loc->y[j] - loc->y[i]) + loc->x[i]) ) {
+    c = !c;
+}
+*/
+
+bool pnpoly(int nvert, Person * points, Person test) {
+    Person loc[] = points;
+    Person pos = test;
+    int nloc = nvert;
+
     int i, j, c = 0;
     for (i = 0, j = nvert-1; i < nvert; j = i++) {
-        if (((verty[i]>testy) !=
-                    (verty[j]>testy)) && (testx <
-                        (vertx[j]-vertx[i]) *
-                        (testy-verty[i]) /
-                        (verty[j]-verty[i]) + vertx[i]) ) {
-            c = !c;
+      if (((loc->y[i] > pos->y) !=
+                (loc->y[j] > pos->y)) && (pos->x <
+                    (loc->x[j] - loc->x[i]) *
+                    (pos->y - loc->y[i]) /
+                    (loc->y[j] - loc->y[i]) + loc->x[i]) ) {
+            c = 1;
         }
     }
     return c;
